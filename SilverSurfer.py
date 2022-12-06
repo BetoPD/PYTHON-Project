c71e513c1f79377c6cb380563eb7e1cf6a08f194
Humberto <humberto.angel.duran@gmail.com>
2022-12-05 18:42:24 -0600

    code academy

--- SilverSurfer.py
+++ SilverSurfer.py
@@ -1,2 +1,10 @@
-print('hola mi nombre es silver surfer')
-print(7**7)
+destinations = ["Paris, France", "Shangai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+
+test_traveler = ["Erwin Wilkes", "Shangai, China", ["historical site", "art"]]
+
+def get_destination_index(destination):
+  for i in range(len(destinations)):
+    if destinations[i] == destination:
+      return i
+
+print(str(get_destination_index("Los Angeles, USA")))
